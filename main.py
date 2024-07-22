e493f31418f07bb5fbae1c7d1901adf8bd719bd6
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-21 19:46:06 -0700

    Added loading the API key through the .env file

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+import os
+
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap5(app)
 
+# read the dotenv
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
 @app.route('/')
 def index():
+    print('Api key: ', API_KEY)
     return {'message': 'Hello, World!'}
