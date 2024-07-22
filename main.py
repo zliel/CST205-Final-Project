e63ee59a499ba8997008173ef5d28a10414ef28f
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-07-21 19:52:16 -0700

    Added a form for the user's search query and a redirect to the "/search_results"

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
 from dotenv import load_dotenv
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
@@ -12,6 +15,17 @@ bootstrap = Bootstrap5(app)
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
+
+class Query(FlaskForm):
+    query = StringField(
+        'Query',
+        validators=[DataRequired()]
+    )
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    form = Query()
+    if form.validate_on_submit():
+        return redirect(f'/book_search_results/{form.query.data}')
     return {'message': 'Hello, World!'}
